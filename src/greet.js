877ee9616d5e849c47345ed7ffc74fadc7a992e5
mohsinkif <mohsinullah68@gmail.com>
2023-12-21 08:51:14 -0800

    https://github.com/mohsinkif/drone2

---
+++ src/greet.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+// Rename the component to start with an uppercase letter (convention for React components)
+const Fun = (props) => <h1>Hello {props.name}</h1>;
+
+export default Fun;
